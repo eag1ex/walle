943038d3306a0a5fc5338b8e7de7626ec2dd58a2
eag1ex <eaglex@eaglex.net>
2020-10-16 11:30:03 +0000

    minor

--- libs/Pre-process.js
+++ libs/Pre-process.js
@@ -1,8 +1,5 @@
-
-// const {onerror,warn} = require('x-utils-es/umd')
 /** 
  * validate if cli entered commands can be executed.
- * 
 */
 const argv = (...args) => {
     const commands = args[0].map((n, i) => i > 1 ? n : null).filter(n => !!(n || "").trim())
